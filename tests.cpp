a4ff875668938eb876cf0ceef913af99a4816d9b
Иван Строгальщиков <71238076+Zivan182@users.noreply.github.com>
2021-12-19 21:20:45 +0300

    Update tests.cpp
    Добавил Google Test

--- tests.cpp
+++ tests.cpp
@@ -1,30 +1,36 @@
+#include <gtest/gtest.h>
 #include "LR.hpp"
 
-void test1() {
+class TestParsing : public ::testing::Test {
+};
+
+TEST(TestParsing, test1) {
 	grammar G1({ {'S', "SaSb"}, {'S', "@"} });
 	LR parser1(G1);
 	assert(parser1.check("abaabb"));
 	assert(parser1.check("aabbabab"));
 	assert(!parser1.check("aabbababbaa"));
-}
 
-void test2() {
+	cout << "Test1 passed\n";
+}
+TEST(TestParsing, test2) {
 	grammar G2({ {'S', "AA"}, {'A', "aA"}, {'A', "b"} });
 	LR parser2(G2);
 	assert(!parser2.check("abaabb"));
 	assert(parser2.check("aaaaabab"));
 	assert(parser2.check("aaabaaaab"));
-}
 
-void test3() {
+	cout << "Test2 passed\n";
+}
+TEST(TestParsing, test3) {
 	grammar G3({ {'S', "A"}, {'S', "S+A"}, {'S', "S-A"}, {'A', "B"}, {'A', "A*B"}, {'B', "(S)"}, {'B', "x"}, {'B', "y"} });
 	LR parser3(G3);
 	assert(parser3.check("(x+y)*(x*x+x*y+y*y)"));
 	assert(parser3.check("(x+y)*(x-y)"));
 	assert(!parser3.check("(x+y)(x*x+x*y+y*y)"));
-}
 
-void test4() {
+}
+TEST(TestParsing, test4) {
     grammar G4({ {'S', "A"}, {'S', "AB"}, {'A', "a"}, {'A', "abc"}, {'B', "bc"} });
     try {
         LR parser4(G4);
@@ -34,9 +40,7 @@ void test4() {
 
 
 
-int main() {
-	test1();
-	test2();
-	test3();
-	test4();
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
