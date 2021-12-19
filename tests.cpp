d81a592efba737702286afb56dd40320f5bf66cc
Иван Строгальщиков <71238076+Zivan182@users.noreply.github.com>
2021-12-19 21:39:13 +0300

    Update tests.cpp
    Изменил assert на ASSERT_TRUE/FALSE.

--- tests.cpp
+++ tests.cpp
@@ -7,34 +7,34 @@ class TestParsing : public ::testing::Test {
 TEST(TestParsing, test1) {
 	grammar G1({ {'S', "SaSb"}, {'S', "@"} });
 	LR parser1(G1);
-	assert(parser1.check("abaabb"));
-	assert(parser1.check("aabbabab"));
-	assert(!parser1.check("aabbababbaa"));
+	ASSERT_TRUE(parser1.check("abaabb"));
+	ASSERT_TRUE(parser1.check("aabbabab"));
+	ASSERT_FALSE(parser1.check("aabbababbaa"));
 
 	cout << "Test1 passed\n";
 }
 TEST(TestParsing, test2) {
 	grammar G2({ {'S', "AA"}, {'A', "aA"}, {'A', "b"} });
 	LR parser2(G2);
-	assert(!parser2.check("abaabb"));
-	assert(parser2.check("aaaaabab"));
-	assert(parser2.check("aaabaaaab"));
+	ASSERT_FALSE(parser2.check("abaabb"));
+	ASSERT_TRUE(parser2.check("aaaaabab"));
+	ASSERT_TRUE(parser2.check("aaabaaaab"));
 
 	cout << "Test2 passed\n";
 }
 TEST(TestParsing, test3) {
 	grammar G3({ {'S', "A"}, {'S', "S+A"}, {'S', "S-A"}, {'A', "B"}, {'A', "A*B"}, {'B', "(S)"}, {'B', "x"}, {'B', "y"} });
 	LR parser3(G3);
-	assert(parser3.check("(x+y)*(x*x+x*y+y*y)"));
-	assert(parser3.check("(x+y)*(x-y)"));
-	assert(!parser3.check("(x+y)(x*x+x*y+y*y)"));
+	ASSERT_TRUE(parser3.check("(x+y)*(x*x+x*y+y*y)"));
+	ASSERT_TRUE(parser3.check("(x+y)*(x-y)"));
+	ASSERT_FALSE(parser3.check("(x+y)(x*x+x*y+y*y)"));
 
 }
 TEST(TestParsing, test4) {
     grammar G4({ {'S', "A"}, {'S', "AB"}, {'A', "a"}, {'A', "abc"}, {'B', "bc"} });
     try {
         LR parser4(G4);
-        assert(false);
+        ASSERT_FALSE(false);
     } catch (bad_grammar) {}
 }
 
